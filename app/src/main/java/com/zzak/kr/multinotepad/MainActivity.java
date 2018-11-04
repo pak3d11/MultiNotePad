f9ac7675b0fc66c4a0b4ceb5aaaca745108818a1
pak3d11 <pak3d1122@gmail.com>
2018-11-05 08:30:08 +0900

    화면 구성코드 작업중 커밋

--- app/src/main/java/com/zzak/kr/multinotepad/MainActivity.java
+++ app/src/main/java/com/zzak/kr/multinotepad/MainActivity.java
@@ -1,13 +1,60 @@
 package com.zzak.kr.multinotepad;
 
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTabLayout();
+    }
+
+    /**
+     * TabLayout, ViewPager 셋팅을 위한 함수
+     */
+    private void setTabLayout() {
+
+        //TabLayout 초기화
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.all_notes));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.collection));
+
+        //ViewPager 초기화
+        viewPager = findViewById(R.id.viewPager);
+
+        //Adapter 셋팅
+        TabPagerAdapter pagerAdapter = new TabPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setOffscreenPageLimit(1);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        //TabSelectListener 셋팅
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
